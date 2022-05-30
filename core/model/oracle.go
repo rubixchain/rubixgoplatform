4cb0b666606cfa1800604f84c099e39769bc9ec9
Priya P <priya.p@rubix.net>
2022-05-30 19:26:24 +0530

    Add model for oracles

---
+++ core/model/oracle.go
@@ -0,0 +1,46 @@
+package model
+
+type Input struct {
+	Server             string        `json:"server"`
+	Function           string        `json:"function"`
+	AddInput           []NodeID      `json:"addInput"`
+	AssignCreditsInput AssignCredits `json:"assignCredits"`
+	UpdateMineInput    UpdateMine    `json:"updateMine"`
+	GetQuorumInput     GetQuorum     `json:"getQuorum"`
+	UpdateQuorumInput  UpdateQuorum  `json:"updateQuorum"`
+}
+
+type TokenID struct {
+	Token string `json:"token"`
+	Level string `json:"level"`
+}
+
+type NodeID struct {
+	PeerID     string `json:"peerid"`
+	DIDHash    string `json:"didHash"`
+	WalletHash string `json:"walletHash"`
+}
+
+type UpdateMine struct {
+	DIDHash string `json:"didhash"`
+	Credits string `json:"credits"`
+}
+
+type AssignCredits struct {
+	DIDHash string `json:"didHash"`
+	Credits string `json:"credits"`
+}
+
+type GetQuorum struct {
+	Receiver   string `json:"receiver"`
+	TokenCount int    `json:"tokencount"`
+	Sender     string `json:"sender"`
+	BetaHash   string `json:"betahash"`
+	GammaHash  string `json:"gammahash"`
+}
+
+type UpdateQuorum struct {
+	CompleteQuorum []string `json:"completequorum"`
+	SignedQuorum   []string `json:"signedquorum"`
+	Status         bool     `json:"status"`
+}
