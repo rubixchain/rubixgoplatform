3f37b3ad7331bcdd087e79f21c7c32514d01ff06
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2023-03-10 00:54:36 +0530

    Updated proof verification

--- core/unpledge/unpledge.go
+++ core/unpledge/unpledge.go
@@ -1,11 +1,16 @@
 package unpledge
 
 import (
+	"bufio"
 	"crypto/sha256"
-	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/EnsurityTechnologies/logger"
@@ -18,177 +23,283 @@ type UnPledge struct {
 	log logger.Logger
 }
 
-type pledgedTokenIDs struct {
-	TokenID string
-}
-
-func (un *UnPledge) listAllPledgedToken(did string) []pledgedTokenIDs {
+func (un *UnPledge) GetPledgedTokenID(did string) ([]string, error) {
 	w := wallet.Wallet{}
-	var u []pledgedTokenIDs
-	pledgedtokenlist, err := w.GetAllPledgedTokens(did)
+	var tokenIDs []string
+	pledgedTokens, err := w.GetAllPledgedTokens(did)
 	if err != nil {
-		un.log.Error("Failed to get tokens:, err")
+		return nil, err
 	}
-	fmt.Println(pledgedtokenlist)
-	for i := range pledgedtokenlist {
-		u = append(u, pledgedTokenIDs{
-			TokenID: pledgedtokenlist[i].TokenID,
-		})
+	for _, token := range pledgedTokens {
+		tokenIDs = append(tokenIDs, token.TokenID)
 	}
-	return u
+	return tokenIDs, nil
 }
 
-func (un *UnPledge) Lastblock(tokenID string) (string, string, error) {
+func (un *UnPledge) GetLastBlockReceiverDID(TokenID string) (string, error) {
 	w := wallet.Wallet{}
-	lastblock := w.GetLatestTokenBlock(tokenID)
-	if lastblock == nil {
-		un.log.Error("latest block for token %s not found", tokenID)
-		return "", "", fmt.Errorf("latest block for token %s not found", tokenID)
+	lastBlock := w.GetLatestTokenBlock(TokenID)
+	if lastBlock == nil {
+		un.log.Error("latest block for token %s not found", TokenID)
+		return "", nil
 	}
-	ReceiverDID := lastblock.GetReceiverDID()
-	transactionID := lastblock.GetTid()
-	return ReceiverDID, transactionID, nil
+	ReceiverDID := lastBlock.GetReceiverDID()
+	return ReceiverDID, nil
 }
 
-func (un *UnPledge) concat(rdid string, pt string) (string, error) {
-	if rdid == "" {
+func (un *UnPledge) GetLastBlockTransactionID(TokenID string) (string, error) {
+	w := wallet.Wallet{}
+	lastBlock := w.GetLatestTokenBlock(TokenID)
+	if lastBlock == nil {
+		un.log.Error("latest block for token %s not found", TokenID)
+		return "", nil
+	}
+	TransactionID := lastBlock.GetTid()
+	return TransactionID, nil
+}
+
+func (un *UnPledge) Concat(TokenID string, ReceiverDID string) (string, error) {
+	if TokenID == "" {
+		err := errors.New("TokenID cannot be empty")
+		un.log.Error(err.Error())
+		return "", err
+	}
+	if ReceiverDID == "" {
 		err := errors.New("ReceiverDID cannot be empty")
 		un.log.Error(err.Error())
 		return "", err
 	}
-	if pt == "" {
-		err := errors.New("PledgeTokenID cannot be empty")
-		un.log.Error(err.Error())
-		return "", err
-	}
-	valueConcated := rdid + pt
+	valueConcated := TokenID + ReceiverDID
 	return valueConcated, nil
 }
 
-func (un *UnPledge) difficultlevel(level int, lastvalue string) string {
-	difflev := DiffLevel[level]
-	result := lastvalue[len(lastvalue)-difflev:]
-	return result
+func (un *UnPledge) CalcSHA_256Hash(input string) string {
+	hash := sha256.Sum256([]byte(input))
+	hashedInput := fmt.Sprintf("%x", hash)
+	return hashedInput
 }
 
-func (un *UnPledge) CalcSHA256Hash(value string) string {
-	hash := sha256.Sum256([]byte(value))
-	hashed := fmt.Sprintf("%x", hash)
-	fmt.Printf("Sha3-256 value is %x", hashed)
-	return hashed
+func (un *UnPledge) CalcSHA_3_256Hash(input string) string {
+	hash := sha3.Sum256([]byte(input))
+	hashedInput := fmt.Sprintf("%x", hash)
+	return hashedInput
 }
 
+func (un *UnPledge) CalcSHA3_256Hash1000Times(input string) string {
+	var hashedInput string
+	for i := 0; i < 1000; i++ {
+		hash := sha3.Sum256([]byte(input))
+		hashedInput = fmt.Sprintf("%x", hash)
+		input = hashedInput
+	}
+	return hashedInput
+}
+
+func (un *UnPledge) getCurrentLevel() int {
+	url := "http://13.76.134.226:9090/getCurrentLevel"
+	resp, err := http.Get(url)
+	if err != nil {
+		un.log.Error("Error:", err)
+		return -1
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		un.log.Error("Error:", err)
+		return -1
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		un.log.Error("Error:", err)
+		return -1
+	}
+
+	level := int(data["level"].(float64))
+	un.log.Debug("Level:", level)
+	return level
+}
+func (un *UnPledge) Difficultlevel() int {
+	cl := un.getCurrentLevel()
+	return DiffLevel[cl]
+}
+func (un *UnPledge) saveProofToFile(proof []string, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	for _, line := range proof {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 func (un *UnPledge) proofcreation(did string) ([]string, error) {
-	pledgedTokens := un.listAllPledgedToken(did)
 	var proof []string
-	for i := range pledgedTokens {
-		tokenID := pledgedTokens[i].TokenID            // Qm1
-		ReciDID, transID, err := un.Lastblock(tokenID) // receiver DID and Transaction ID
+	tokenIDs, err := un.GetPledgedTokenID(did)
+	if err != nil {
+		un.log.Error("Error fetching Pledged Tokens ")
+	}
+	for i := range tokenIDs {
+		TokenID := tokenIDs[i]
+		ReceiverDID, err := un.GetLastBlockReceiverDID(TokenID)
+
 		if err != nil {
-			un.log.Error("error getting last block for token %s: %s", tokenID, err.Error())
+			un.log.Error("Unable to fetch Receiver DID from last block")
 			continue
 		}
-		un.log.Debug("Token is %s", tokenID)
-		un.log.Debug("Receiver DID is %s", ReciDID)
-		un.log.Debug("Transaction ID is %s", transID)
 
-		value, err := un.concat(tokenID, ReciDID) // concat ReceiverDID and TokenID
+		TransactionID, err := un.GetLastBlockTransactionID(TokenID)
 		if err != nil {
-			un.log.Error("error concatenating values: %s", err.Error())
+			un.log.Error("Unable to fetch Transaction ID from last block")
 			continue
 		}
-		ValueToBeHashed := un.CalcSHA256Hash(value) // value to be hashed
-		val := un.difficultlevel(4, transID)
-		hashLen := len(ValueToBeHashed)
-		valLen := len(val)
 
-		//fetch level
+		ValueToBeHashed, err := un.Concat(TokenID, ReceiverDID)
+		if err != nil {
+			un.log.Error("Unable Concat Receiver DID and Token ID")
+		}
+
+		dl := un.Difficultlevel()
+
+		proof = append(proof, strconv.Itoa(dl))
+
+		ValueHashed := un.CalcSHA_256Hash(ValueToBeHashed)
+		proof = append(proof, ValueHashed)
+
+		SuffixTransactionID := TransactionID[len(TransactionID)-dl:]
+
+		count := 1
 
 		for {
-			count := 0
-			// Compute the SHA3-256 hash of the string
-			hash := sha3.Sum256([]byte(ValueToBeHashed))
 
-			// Convert the hash to a hex-encoded string
-			hashString := hex.EncodeToString(hash[:])
-			proof = append(proof, hashString)
+			targetHash := un.CalcSHA_3_256Hash(ValueHashed)
 
-			// Check if the last n characters of the hash match the desired value
-			if len(hashString) >= hashLen && valLen <= hashLen {
-				hashSuffix := hashString[hashLen-valLen:]
-				if hashSuffix == val {
-					un.log.Debug("hashing completed at %d", count)
-
-					// Append the hashed value to the proof array
-					if count%1000 == 0 {
-						proof = append(proof, hashString)
-					}
-					break
-				}
+			if count%1000 == 0 {
+				proof = append(proof, targetHash)
 			}
-			// Update the string to be hashed in the next iteration of the loop
+
+			SuffixTarget := targetHash[len(ValueHashed)-dl:]
+
+			if SuffixTarget == SuffixTransactionID {
+				proof = append(proof, targetHash)
+
+				un.log.Debug("Hashing completed at ", count)
+
+				un.log.Debug("Proof hash is ", targetHash, " Transaction ID is ", TransactionID)
+
+				break
+			}
+
 			count++
-			ValueToBeHashed = hashString
+			ValueHashed = targetHash
+		}
+
+		pf := un.saveProofToFile(proof, tokenIDs[i]+".proof")
+		if pf != nil {
+			un.log.Error("Unable to save proof.json")
 		}
 	}
 	return proof, nil
 }
 
-func (un *UnPledge) proofverification(proof []string) error {
-	var token string
-	var recDID string
-	if len(proof) < 1 {
-		un.log.Error("Proof is empty")
+func (un *UnPledge) proofverification(tokenID string, proof []string) error {
+
+	for {
+		TokenID := tokenID
+		ReceiverDID, err := un.GetLastBlockReceiverDID(TokenID)
+		if err != nil {
+			un.log.Error("Unable to fetch Receiver DID from last block")
+			break
+		}
+		TransactionID, err := un.GetLastBlockTransactionID(TokenID)
+		if err != nil {
+			un.log.Error("Unable to fetch Transaction ID from last block")
+		}
+
+		ValueToBeHashed, err := un.Concat(TokenID, ReceiverDID)
+		if err != nil {
+			un.log.Error("Unable to verify proof")
+			break
+		}
+
+		valueHashed := un.CalcSHA_256Hash(ValueToBeHashed)
+		if proof[0] == "" {
+			un.log.Error("Unable to verify proof")
+			break
+		}
+		dl := un.Difficultlevel()
+		if proof[0] != strconv.Itoa(dl) {
+			un.log.Error("Unable to verify proof")
+			break
+		}
+
+		if proof[1] != valueHashed {
+			un.log.Error("Unable to verify proof")
+			break
+		}
+
+		proofToVerify := proof[1:] // Exculding firstline (Difficuilty level)
+		lenProof := len(proof)
+		lenProoftoVerify := len(proofToVerify)
+		l := lenProoftoVerify / 2
+
+		firstHalf := proof[1 : l-1]
+		secondHalf := proof[l+1 : lenProoftoVerify-1]
+
+		rand.Seed(time.Now().UnixNano())
+
+		randIndexInFH := rand.Intn(len(firstHalf) - 1)
+		randIndexInSH := rand.Intn(len(secondHalf) - 1)
+
+		RandomHashInFH := firstHalf[randIndexInFH]
+		RandomHashInSH := secondHalf[randIndexInSH]
+
+		TargetHashInFH := firstHalf[randIndexInFH+1]
+		TargetHashInSH := secondHalf[randIndexInSH+1]
+
+		if un.CalcSHA3_256Hash1000Times(RandomHashInFH) != TargetHashInFH || un.CalcSHA3_256Hash1000Times(RandomHashInSH) != TargetHashInSH {
+			un.log.Error("Unable to verify proof")
+			break
+		}
+		var c int
+		counter := 0
+		target := proof[lenProof-3]
+
+		SuffixLasthash := TransactionID[len(TransactionID)-dl:]
+
+		for {
+			targetHash := un.CalcSHA_3_256Hash(target)
+			SuffixTarget := targetHash[len(targetHash)-dl:]
+
+			if SuffixTarget == SuffixLasthash {
+				c = counter
+				break
+			}
+			counter++
+			target = targetHash
+		}
+		if c > 1000 {
+			un.log.Error("Unable to verify proof")
+			break
+		} else {
+			un.log.Debug("Proof Verified")
+		}
+		break
 	}
-
-	valToBeHash, err := un.concat(recDID, token)
-
-	if err != nil {
-		un.log.Error("Unable to concat ReciDID and token ID")
-	}
-
-	hash := sha256.Sum256([]byte(valToBeHash))
-	hashString := hex.EncodeToString(hash[:])
-
-	firstlineProof := proof[0]
-
-	if firstlineProof == "" {
-		un.log.Error("Firstline of proof is empty")
-	}
-	if firstlineProof != string(hashString) {
-		un.log.Error("Firstline of proof mismatch")
-	}
-	prooflen := len(proof[1:])
-	l := prooflen / 2
-
-	firstHalf := proof[1:l]
-	secondHalf := proof[l+1:]
-	//middle := proof[l]
-
-	rand.Seed(time.Now().UnixNano())
-
-	randIndexFH := rand.Intn(len(firstHalf))
-	randIndexSH := rand.Intn(len(secondHalf))
-
-	var randFH [32]byte
-	var randSH [32]byte
-
-	for i := 0; i < 1000; i++ {
-		randFH = sha256.Sum256([]byte(firstHalf[randIndexFH]))
-	}
-	hashrandFH := hex.EncodeToString(randFH[:])
-
-	for i := 0; i < 1000; i++ {
-		randSH = sha256.Sum256([]byte(secondHalf[randIndexSH]))
-	}
-	hashrandSH := hex.EncodeToString(randSH[:])
-
-	firstHalfHash := firstHalf[randIndexFH+1]
-	secondHalfHash := secondHalf[randIndexSH+1]
-
-	if hashrandFH != firstHalfHash || hashrandSH != secondHalfHash {
-		un.log.Error("Unable to verify proof")
-	}
-	un.log.Debug("Proof Verified")
 	return nil
 }
