e8b9b5d15bcc8d0d2383483962e18ddd4c251cb8
Kiran H <kiran.h@rubix.network>
2023-03-23 12:06:54 +0530

    added logger

--- core/wallet/token_chain.go
+++ core/wallet/token_chain.go
@@ -247,7 +247,7 @@ func (w *Wallet) addBlock(tt int, token string, b *block.Block) error {
 			return err
 		}
 		if lbn+1 != bn {
-			w.log.Error("Invalid block number, sequence missing", "lbn", lbn, "bn", bn)
+			w.log.Error("Invalid block number, sequence missing", "lbn", lbn, "bn", bn, "token", token)
 			return fmt.Errorf("invalid block number, sequence missing")
 		}
 	}
