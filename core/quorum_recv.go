5f0f82f9343ac6e08eb4f6b07df95604d8ab43fd
Kiran H <kiran.h@rubix.network>
2023-05-23 16:25:39 +0530

    tokenstate validation
    removed comments
    
    addded debug statements
    
    added tokenstatedata post chekc to be pinned
    
    remove quorumpeers from dht list
    
    removed comments
    
    updated return status of tokestatepincheck
    
    boolean value and variable updated checkTokenState
    
    removed unsued code
    
    variable name update
    
    exit checkTokenState if err triggered add retun

--- core/quorum_recv.go
+++ core/quorum_recv.go
@@ -142,6 +142,42 @@ func (c *Core) quorumRBTConsensus(req *ensweb.Request, did string, qdc didcrypto
 		crep.Message = "Token ownership check failed"
 		return c.l.RenderJSON(req, &crep, http.StatusOK)
 	}
+
+	//Token state check and pinning
+	/*
+		1. get the latest block from token chain,
+		2. retrive the Block Id
+		3. concat token id and blockId
+		4. add to ipfs
+		5. check for pin and if none pin the content
+		6. if pin exist , exit with error token state exhauste
+	*/
+
+	tokenStateCheckResult := make([]TokenStateCheckResult, len(ti))
+	c.log.Debug("entering validation to check if token state is exhausted, ti len", len(ti))
+	for i := range ti {
+		wg.Add(1)
+		go c.checkTokenState(ti[i].Token, did, i, tokenStateCheckResult, &wg, cr.QuorumList)
+	}
+	wg.Wait()
+
+	for i := range tokenStateCheckResult {
+		if tokenStateCheckResult[i].Error != nil {
+			c.log.Error("Error occured", "error", err)
+			crep.Message = "Error while cheking Token State Message : " + tokenStateCheckResult[i].Message
+			return c.l.RenderJSON(req, &crep, http.StatusOK)
+		}
+		if tokenStateCheckResult[i].Exhausted {
+			c.log.Debug("Token state has been exhausted, Token being Double spent:", tokenStateCheckResult[i].Token)
+			crep.Message = tokenStateCheckResult[i].Message
+			return c.l.RenderJSON(req, &crep, http.StatusOK)
+		}
+		c.log.Debug("Token", tokenStateCheckResult[i].Token, "Message", tokenStateCheckResult[i].Message)
+	}
+	c.log.Debug("Proceeding to pin token state to prevent double spend")
+	c.pinTokenState(tokenStateCheckResult, did)
+	c.log.Debug("Finished Tokenstate check")
+
 	//check if token is pledgedtoken
 	wt := sc.GetTransTokenInfo()
 
--- core/quorum_validation.go
+++ core/quorum_validation.go
@@ -2,14 +2,25 @@ package core
 
 import (
 	"bytes"
+	"fmt"
+	"sync"
 
 	ipfsnode "github.com/ipfs/go-ipfs-api"
 	"github.com/rubixchain/rubixgoplatform/block"
 	"github.com/rubixchain/rubixgoplatform/contract"
+	"github.com/rubixchain/rubixgoplatform/core/wallet"
 	"github.com/rubixchain/rubixgoplatform/did"
 	"github.com/rubixchain/rubixgoplatform/token"
 	"github.com/rubixchain/rubixgoplatform/util"
 )
+
+type TokenStateCheckResult struct {
+	Token                 string
+	Exhausted             bool
+	Error                 error
+	Message               string
+	tokenIDTokenStateData string
+}
 
 func (c *Core) validateTokenOwnership(cr *ConensusRequest, sc *contract.Contract) bool {
 	ti := sc.GetTransTokenInfo()
@@ -166,4 +177,99 @@ func (c *Core) getUnpledgeId(wt string) string {
 		return ""
 	}
 	return b.GetUnpledgeId()
+}
+
+/*
+ * Function to check whether the TokenState is pinned or not
+ * Input tokenId, index, resultArray, waitgroup,quorumList
+ */
+func (c *Core) checkTokenState(tokenId, did string, index int, resultArray []TokenStateCheckResult, wg *sync.WaitGroup, quorumList []string) {
+	defer wg.Done()
+	var result TokenStateCheckResult
+	result.Token = tokenId
+	//get the latest blockId i.e. latest token state
+	block := c.w.GetLatestTokenBlock(tokenId, token.RBTTokenType)
+	if block == nil {
+		c.log.Error("Invalid token chain block")
+		result.Error = fmt.Errorf("Invalid token chain block")
+		result.Message = "Invalid token chain block"
+		resultArray[index] = result
+		return
+	}
+	blockId, err := block.GetBlockID(tokenId)
+	if err != nil {
+		c.log.Error("Error fetching block Id", err)
+		result.Error = err
+		result.Message = "Error fetching block Id"
+		resultArray[index] = result
+		return
+	}
+	//concat tokenId and BlockID
+	tokenIDTokenStateData := tokenId + blockId
+	tokenIDTokenStateBuffer := bytes.NewBuffer([]byte(tokenIDTokenStateData))
+
+	//add to ipfs get only the hash of the token+tokenstate
+	tokenIDTokenStateHash, err := c.ipfs.Add(tokenIDTokenStateBuffer, ipfsnode.Pin(false), ipfsnode.OnlyHash(true))
+	if err != nil {
+		c.log.Error("Error adding data to ipfs", err)
+		result.Error = err
+		result.Message = "Error adding data to ipfs"
+		resultArray[index] = result
+		return
+	}
+
+	//check dht to see if any pin exist
+	list, err1 := c.GetDHTddrs(tokenIDTokenStateHash)
+	//try to call ipfs cat to check if any one has pinned the state i.e \
+	if err1 != nil {
+		c.log.Error("Error fetching content for the tokenstate ipfs hash :", tokenIDTokenStateHash, "Error", err)
+		result.Error = err
+		result.Message = "Error fetching content for the tokenstate ipfs hash : " + tokenIDTokenStateHash
+		resultArray[index] = result
+		return
+	}
+	//remove ql peer ids from list
+	qPeerIds := make([]string, 0)
+
+	for i := range quorumList {
+		pId, _, ok := util.ParseAddress(quorumList[i])
+		if !ok {
+			c.log.Error("Error parsing addressing")
+			result.Error = err
+			result.Message = "Error parsing addressing"
+			resultArray[index] = result
+			return
+		}
+		qPeerIds = append(qPeerIds, pId)
+	}
+	updatedList := c.removeStrings(list, qPeerIds)
+	//if pin exist abort
+	if len(updatedList) != 0 {
+		c.log.Debug("Token state is exhausted, Token is being Double spent")
+		result.Exhausted = true
+		result.Error = nil
+		result.Message = "Token state is exhausted, Token is being Double spent"
+		resultArray[index] = result
+		return
+	}
+	c.log.Debug("Token state is not exhausted, Unique Txn")
+	result.Error = nil
+	result.Message = "Token state is free, Unique Txn"
+	result.tokenIDTokenStateData = tokenIDTokenStateData
+	resultArray[index] = result
+}
+
+func (c *Core) pinTokenState(tokenStateCheckResult []TokenStateCheckResult, did string) {
+	for i := range tokenStateCheckResult {
+		tokenIDTokenStateBuffer := bytes.NewBuffer([]byte(tokenStateCheckResult[i].tokenIDTokenStateData))
+		id, err := c.w.Add(tokenIDTokenStateBuffer, did, wallet.QuorumRole)
+		if err != nil {
+			c.log.Error("Error triggered while adding token state", err)
+		}
+		_, err1 := c.w.Pin(id, wallet.QuorumRole, did)
+		if err1 != nil {
+			c.log.Error("Error triggered while pinning token state", err)
+		}
+		c.log.Debug("token state pinned", id)
+	}
 }
