8503aece92d9d320c79000e6dcec7be9b323a369
Kiran H <kiran.h@rubix.network>
2023-06-13 13:49:27 +0530

    method to deploy smart contract token

---
+++ core/smart_contract_token_operations.go
@@ -0,0 +1,8 @@
+package core
+
+/*
+*This method Deploys the smart contract Token that was generated after passing through consensus
+ */
+func (c *Core) deploySmartContractToken(smartContractTokenHash string) {
+
+}
