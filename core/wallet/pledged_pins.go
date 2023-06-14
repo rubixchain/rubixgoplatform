bb6c7ee49e80e99bae5b57ac6d1807bb4dc498da
Aparna Ullas <aparna.u@rubix.net>
2023-06-14 17:04:57 +0200

    Pinning Pledged tokens
    New class to stored details of pins of token by quorums who pledged their tokens

---
+++ core/wallet/pledged_pins.go
@@ -0,0 +1,18 @@
+package wallet
+
+import "time"
+
+type PledgePinsDetails struct {
+	QuorumDID string    `gorm:"column:date_time"`
+	TokenID   string    `gorm:"column:status"`
+	EpochTime time.Time `gorm:"column:epoch_time"`
+}
+
+func (w *Wallet) AddPins(pd *PledgePinsDetails) error {
+	err := w.s.Write(PledgePinsStorage, pd)
+	if err != nil {
+		w.log.Error("Failed to store pins", "err", err)
+		return err
+	}
+	return nil
+}
