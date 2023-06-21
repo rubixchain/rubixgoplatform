ad85ec2ca77049d9c28553f9e26b451cd0d12bce
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2023-06-21 12:59:33 +0530

    Add PubSub functions in server

---
+++ server/contract.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"github.com/EnsurityTechnologies/ensweb"
+	"github.com/rubixchain/rubixgoplatform/core/model"
+)
+
+func (s *Server) APIPublishEvent(req *ensweb.Request) *ensweb.Result {
+	// nc is new contract
+	var ne model.NewContractEvent
+	err := s.ParseJSON(req, &ne)
+	if err != nil {
+		return s.BasicResponse(req, false, "Failed to parse input", nil)
+	}
+
+	go s.c.PublishNewEvent(&ne)
+	return s.BasicResponse(req, true, "", nil)
+}
+func (s *Server) APISubscribecontract(req *ensweb.Request) *ensweb.Result {
+	var ns model.NewSubcription
+	err := s.ParseJSON(req, &ns)
+	if err != nil {
+		return s.BasicResponse(req, false, "Failed to parse input", nil)
+	}
+	topic := ns.Contract
+	go s.c.SubsribeContractSetup(topic)
+	return s.BasicResponse(req, true, "", nil)
+}
