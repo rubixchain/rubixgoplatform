47ad3121cfffa1762c18dae13031b801bb0639f0
Kiran H <kiran.h@rubix.network>
2023-08-30 18:03:11 +0530

    TokenStatePinDetails update TokenStateProviderMap

--- core/wallet/token_state_pin_storage.go
+++ core/wallet/token_state_pin_storage.go
@@ -1,8 +1,10 @@
 package wallet
 
-type TokenStatePinDetails struct {
-	TokenStateId   string
-	TokenStateData string
-	TokenId        string
-	DID            string
+type TokenStateProviderMap struct {
+	TokenStateId   string `gorm:"column:token_state_id;primaryKey"`
+	TokenStateData string `gorm:"column:token_state_data"`
+	TokenId        string `gorm:"column:token_id"`
+	DID            string `gorm:"column:did"`
+	FuncID         int    `gorm:"column:func_id"`
+	Role           int    `gorm:"column:role"`
 }
