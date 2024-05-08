586b7a5afbae13b5fc37b96331c558902fcdcc32
Maneesha-rubix <maneesha.p@rubix.net>
2024-05-08 14:18:09 +0530

    ping peer to get peer did type

--- core/ping.go
+++ core/ping.go
@@ -29,6 +29,11 @@ func (c *Core) CheckQuorumStatusSetup() {
 	c.l.AddRoute(APICheckQuorumStatusPath, "GET", c.CheckQuorumStatusResponse)
 }
 
+// GetPeerdidTypeSetup will setup the ping route
+func (c *Core) GetPeerdidTypeSetup() {
+	c.l.AddRoute(APIGetPeerDIDTypePath, "GET", c.GetPeerdidTypeResponse)
+}
+
 // PingRecevied is the handler for ping request
 func (c *Core) PingRecevied(req *ensweb.Request) *ensweb.Result {
 	c.log.Info("Ping Received")
@@ -97,4 +102,49 @@ func (c *Core) CheckQuorumStatus(peerID string, did string) (string, bool, error
 		return "Send Json Request error ", false, err
 	}
 	return checkQuorumStatusResponse.Message, checkQuorumStatusResponse.Status, nil
+}
+
+// CheckQuorumStatusResponse is the handler for CheckQuorumStatus request
+func (c *Core) GetPeerdidTypeResponse(req *ensweb.Request) *ensweb.Result { //PingRecevied
+	did := c.l.GetQuerry(req, "did")
+	c.log.Info("Fetching peer did type from peer")
+	resp := &model.GetDIDTypeResponse{
+		BasicResponse: model.BasicResponse{
+			Status: false,
+		},
+	}
+
+	dt, err := c.w.GetDID(did)
+	if err != nil {
+		c.log.Error("Couldn't fetch did type from DID Table", "error", err)
+		resp.Message = "Couldn't fetch did type for did: " + did
+		resp.Status = false
+		resp.DidType = -1
+		return c.l.RenderJSON(req, &resp, http.StatusOK)
+	} else {
+		resp.DidType = dt.Type
+		resp.Status = true
+		resp.Message = "successfully fetched did type"
+		return c.l.RenderJSON(req, &resp, http.StatusOK)
+	}
+
+}
+
+// GetPeerdidType will ping the peer & get the did type
+func (c *Core) GetPeerdidType_fromPeer(peerID string, did string) (int, string, error) {
+	q := make(map[string]string)
+	p, err := c.pm.OpenPeerConn(peerID, did, c.getCoreAppName(peerID))
+	if err != nil {
+		return -1, "Quorum Connection Error", fmt.Errorf("quorum connection error")
+	}
+
+	// Close the p2p before exit
+	defer p.Close()
+	q["did"] = did
+	var getPeerdidTypeResponse model.GetDIDTypeResponse
+	err = p.SendJSONRequest("GET", APIGetPeerDIDTypePath, q, nil, &getPeerdidTypeResponse, false, 2*time.Minute)
+	if err != nil {
+		return -1, "Send Json Request error ", err
+	}
+	return getPeerdidTypeResponse.DidType, getPeerdidTypeResponse.Message, nil
 }
