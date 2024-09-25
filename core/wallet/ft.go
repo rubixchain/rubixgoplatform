04423f2c10a843eb6a0a8767cee4f23e9a59238d
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2024-09-25 18:15:45 +0530

    Update FT and FT Tokens Table structure

--- core/wallet/ft.go
+++ core/wallet/ft.go
@@ -3,7 +3,8 @@ package wallet
 type FTToken struct {
 	TokenID        string  `gorm:"column:token_id;primaryKey"`
 	FTName         string  `gorm:"column:ft_name"`
-	DID            string  `gorm:"column:did"`
+	DID            string  `gorm:"column:owner_did"`
+	CreatorDID     string  `gorm:"column:creator_did"`
 	TokenStatus    int     `gorm:"column:token_status"`
 	TokenValue     float64 `gorm:"column:token_value"`
 	TokenStateHash string  `gorm:"column:token_state_hash"`
@@ -11,6 +12,8 @@ type FTToken struct {
 }
 
 type FT struct {
-	FTName  string `gorm:"column:ft_name;primaryKey"`
-	FTCount int    `gorm:"column:ft_count"`
+	ID         string `gorm:"column:id;primaryKey;autoIncrement"`
+	FTName     string `gorm:"column:ft_name"`
+	FTCount    int    `gorm:"column:ft_count"`
+	CreatorDID string `gorm:"column:creator_did"`
 }
