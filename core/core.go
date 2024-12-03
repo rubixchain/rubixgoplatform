d5b8e105904f6aa1b595081721638666f8e8d26d
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2024-12-03 12:11:35 +0530

    Add passing of address in InitConfig

--- core/core.go
+++ core/core.go
@@ -121,12 +121,12 @@ type Core struct {
 	noBalanceQuorumCount int
 }
 
-func InitConfig(configFile string, encKey string, node uint16) error {
+func InitConfig(configFile string, encKey string, node uint16, addr string) error {
 	if _, err := os.Stat(configFile); errors.Is(err, os.ErrNotExist) {
 		nodePort := NodePort + node
 		portOffset := MaxPeerConn * node
 		cfg := config.Config{
-			NodeAddress: "localhost",
+			NodeAddress: addr,
 			NodePort:    fmt.Sprintf("%d", nodePort),
 			DirPath:     "./",
 			CfgData: config.ConfigData{
