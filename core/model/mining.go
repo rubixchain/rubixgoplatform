a124274b0518af6fce03fd4a1be7d1e087419726
KryptSai <saibaba.k@rubix.net>
2025-02-20 17:49:37 +0530

    adding PledgeHistory struct in model/mining.go

---
+++ core/model/mining.go
@@ -0,0 +1,22 @@
+package model
+
+type MiningRequest struct {
+	MinerDid     string `json:"Minor"`
+	TokenCredits int    `json:"credits"`
+	Password     string `json:"password"`
+}
+
+type PledgeHistory struct {
+	QuorumDID            string  `gorm:"column:quorum_did"`
+	TransactionID        string  `gorm:"column:transaction_id"`
+	TransactionType      int     `gorm:"column:transaction_type"`
+	TransferTokenID      string  `gorm:"column:transfer_tokens_id"`
+	TransferTokenType    int     `gorm:"column:transfer_tokens_type"`
+	TransferTokenValue   float64 `gorm:"column:transfer_token_value"`
+	TransferBlockID      string  `gorm:"column:transfer_block_number_and_id"`
+	LatestTokenStateHash string  `gorm:"column:latest_tokenstate_hash"`
+	Epoch                int     `gorm:"column:epoch"`
+	NextBlockEpoch       int64   `gorm:"column:next_epoch"`
+	TokenCredit          int     `gorm:"column:token_credit"`
+	TokenCreditStatus    int     `gorm:"column:token_credit_status"`
+}
