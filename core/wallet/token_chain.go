6f422cde505e60146b80b2d23e69468243e9bcc9
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2025-11-13 19:43:42 +0530

    Fix pushing block to explorer error

--- core/wallet/token_chain.go
+++ core/wallet/token_chain.go
@@ -632,7 +632,7 @@ func (w *Wallet) addBlock(token string, b *block.Block) error {
 
 // addFullNodeBlock will write block into fullnode-storage
 func (w *Wallet) addFullNodeBlock(token string, b *block.Block) error {
-	// defer w.notifyExplorerServer(b)
+	defer w.notifyExplorerServer(b)
 	opt := &opt.WriteOptions{
 		Sync: true,
 	}
