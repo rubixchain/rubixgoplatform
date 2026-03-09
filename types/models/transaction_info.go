cd66605e4a0bd68edce5aa73d851d0c1a20dc113
Arnab Ghose <arnab.ghose128@gmail.com>
2026-03-09 12:14:38 +0530

    refactored TransactionInfo struct with new TokenInfo struct for committedTokens and quorum pledge tokens

--- types/models/transaction_info.go
+++ types/models/transaction_info.go
@@ -1,25 +1,25 @@
 package models
 
 type TransactionInfo struct {
-	Initiator       string              `json:"initiator"`
-	Owner           string              `json:"owner"`
-	Epoch           int                 `json:"epoch"`
-	Network         string              `json:"network"`
-	Tokens          *TokenInfo          `json:"tokens"`
-	CommittedTokens []string            `json:"committedTokens"`
-	Quorums         map[string][]string `json:"quorums"`
-	Memo            string              `json:"memo"`
-	Data            string              `json:"data"`
+	Initiator       string                 `json:"initiator"`
+	Owner           string                 `json:"owner"`
+	Epoch           int                    `json:"epoch"`
+	Network         string                 `json:"network"`
+	Tokens          *TransactionTokens     `json:"tokens"`
+	CommittedTokens []*TokenInfo            `json:"committedTokens"`
+	Quorums         map[string][]*TokenInfo `json:"quorums"`
+	Memo            string                 `json:"memo"`
+	Data            string                 `json:"data"`
+}
+
+type TransactionTokens struct {
+	RBT           []*TokenInfo `json:"rbt"`
+	NFT           []*TokenInfo `json:"nft"`
+	FT            []*TokenInfo `json:"ft"`
+	SmartContract []*TokenInfo `json:"smart_contract"`
 }
 
 type TokenInfo struct {
-	RBT           []*TokenInfoDetails `json:"rbt"`
-	NFT           []*TokenInfoDetails `json:"nft"`
-	FT            []*TokenInfoDetails `json:"ft"`
-	SmartContract []*TokenInfoDetails `json:"smart_contract"`
-}
-
-type TokenInfoDetails struct {
 	TokenID               string `json:"tokenId"`
 	PreviousTransactionID string `json:"previousTransactionID"`
 }
