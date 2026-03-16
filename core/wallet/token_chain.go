642fdf43bfbc9b4cc77f8e67e61efc96f728bf74
Maneesha Panda <maneesha.p@rubix.net>
2026-03-16 18:10:46 +0530

    functions to get: transactionId by index, latest transactionId of a given token, and role in latest transaction of a given token

--- core/wallet/token_chain.go
+++ core/wallet/token_chain.go
@@ -19,27 +19,89 @@ func (w *Wallet) GetTokenChainByTokenID(tokenID string) ([]models.TokenChain, er
 	return pgx.CollectRows(rows, pgx.RowToStructByName[models.TokenChain])
 }
 
-func (w *Wallet) GetLatestTransactionAndRoleByTokenID(tokenID string) (*models.Transactions, int16, error) {
-	row := w.db.Pool().QueryRow(w.Ctx,
-		`SELECT transaction_id, role FROM tokenchain WHERE token_id = $1 ORDER BY height DESC LIMIT 1`, tokenID,
+// get latest transaction id of the given token id
+func (w *Wallet) GetLatestTransactionIdByTokenId(tokenID string) (string, error) {
+	row, err := w.db.Pool().Query(w.Ctx,
+		`SELECT index[array_upper(index, 1)]
+			FROM tokenchain_index
+			WHERE token_id = $1`, tokenID,
 	)
-
-	var txID string
-	var tokenRoleInTx int16
-	if err := row.Scan(&txID, &tokenRoleInTx); err != nil {
-		if err == pgx.ErrNoRows {
-			return nil, -1, nil
-		}
-		return nil, -1, fmt.Errorf("GetLatestTransactionByTokenID scan: %w", err)
-	}
-
-	tx, err := w.GetTransactionByID(txID)
 	if err != nil {
-		return nil, -1, fmt.Errorf("GetLatestTransactionByTokenID GetTransactionByID: %w", err)
+		return "", fmt.Errorf("GetTokenChainByTokenID: %w", err)
+	}
+	var latestTxnIdIndex int
+	if err := row.Scan(&latestTxnIdIndex); err != nil {
+		if err == pgx.ErrNoRows {
+			return "", fmt.Errorf("latest transaction id not found for token %s", tokenID)
+		}
+		return "", fmt.Errorf("GetLatestTransactionIdByTokenId scan: %w", err)
 	}
 
-	return tx, tokenRoleInTx, nil
+	return w.GetTransactionIdByIndex(int16(latestTxnIdIndex))
 }
+
+// ********TODO: provide index column name as in tokenchain table
+// get transaction id by Index id
+func (w *Wallet) GetTransactionIdByIndex(index int16) (string, error) {
+	row, err := w.db.Pool().Query(w.Ctx,
+		`SELECT transaction_id
+		FROM tokenchain
+		WHERE index = $1`, index,
+	)
+	if err != nil {
+		return "", fmt.Errorf("GetTokenChainByTokenID: %w", err)
+	}
+	var txnId string
+	if err := row.Scan(&txnId); err != nil {
+		if err == pgx.ErrNoRows {
+			return "", fmt.Errorf("transaction id not found for the index %d", index)
+		}
+		return "", fmt.Errorf("GetTransactionIdByIndex scan: %w", err)
+	}
+	return txnId, nil
+}
+
+// get token role in latest transaction of the given token id
+func (w *Wallet) GetRoleOfTokenIdInLatestTxn(tokenID string) (int16, error) {
+	row, err := w.db.Pool().Query(w.Ctx,
+		`SELECT role
+		FROM tokenchain
+		WHERE token_id = $1`, tokenID,
+	)
+	if err != nil {
+		return -1, fmt.Errorf("GetTokenChainByTokenID: %w", err)
+	}
+	var role int16
+	if err := row.Scan(&role); err != nil {
+		if err == pgx.ErrNoRows {
+			return -1, fmt.Errorf("latest token role not found for token %s", tokenID)
+		}
+		return -1, fmt.Errorf("GetRoleOfTokenIdInLatestTxn scan: %w", err)
+	}
+	return role, nil
+}
+
+// func (w *Wallet) GetLatestTransactionAndRoleByTokenID(tokenID string) (*models.Transactions, int16, error) {
+// 	row := w.db.Pool().QueryRow(w.Ctx,
+// 		`SELECT transaction_id, role FROM tokenchain WHERE token_id = $1 ORDER BY height DESC LIMIT 1`, tokenID,
+// 	)
+
+// 	var txID string
+// 	var tokenRoleInTx int16
+// 	if err := row.Scan(&txID, &tokenRoleInTx); err != nil {
+// 		if err == pgx.ErrNoRows {
+// 			return nil, -1, nil
+// 		}
+// 		return nil, -1, fmt.Errorf("GetLatestTransactionByTokenID scan: %w", err)
+// 	}
+
+// 	tx, err := w.GetTransactionByID(txID)
+// 	if err != nil {
+// 		return nil, -1, fmt.Errorf("GetLatestTransactionByTokenID GetTransactionByID: %w", err)
+// 	}
+
+// 	return tx, tokenRoleInTx, nil
+// }
 
 func (w *Wallet) AddTokenChainEntry(entry *models.TokenChain) error {
 	_, err := w.db.Pool().Exec(w.Ctx, `
