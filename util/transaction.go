78ea40ad37f48272e1e0891802bf04dc73bcbd2b
Maneesha Panda <maneesha.p@rubix.net>
2026-03-17 11:40:26 +0530

    function to converttransactions into and from byte arrays

--- util/transaction.go
+++ util/transaction.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"io"
 
 	"github.com/rubixchain/rubixgoplatform/constants"
 	"github.com/rubixchain/rubixgoplatform/types/models"
@@ -11,9 +14,73 @@ import (
 func GetTransactionID(txInfo *models.TransactionInfo) (string, error) {
 	txInfoBytes, err := json.Marshal(txInfo)
 	if err != nil {
-		return "", fmt.Errorf("GetTransactionID: failed to marshal TransactionInfo, err: %v", er)
+		return "", fmt.Errorf("GetTransactionID: failed to marshal TransactionInfo, err: %v", err)
 	}
 
-	txHashBytes := CalculateHash(txInfoBytes, constants.HashAlgorithm_SHA3_256)	
+	txHashBytes := CalculateHash(txInfoBytes, constants.HashAlgorithm_SHA3_256)
 	return string(txHashBytes), nil
+}
+
+// TransactionToBytes converts transaction struct into a byte array
+func TransactionToBytes(txn *models.Transactions) ([]byte, error) {
+	var buf bytes.Buffer
+
+	// Write ID (prefixed with its length as uint32)
+	idBytes := []byte(txn.ID)
+	if err := binary.Write(&buf, binary.LittleEndian, uint32(len(idBytes))); err != nil {
+		return nil, err
+	}
+	buf.Write(idBytes)
+
+	// Write Info (prefixed with its length as uint32)
+	if err := binary.Write(&buf, binary.LittleEndian, uint32(len(txn.Info))); err != nil {
+		return nil, err
+	}
+	buf.Write(txn.Info)
+
+	// Write Signature (prefixed with its length as uint32)
+	if err := binary.Write(&buf, binary.LittleEndian, uint32(len(txn.Signature))); err != nil {
+		return nil, err
+	}
+	buf.Write(txn.Signature)
+
+	return buf.Bytes(), nil
+}
+
+func TransactionFromBytes(data []byte) (*models.Transactions, error) {
+	buf := bytes.NewReader(data)
+	t := &models.Transactions{}
+
+	// Read ID
+	var idLen uint32
+	if err := binary.Read(buf, binary.LittleEndian, &idLen); err != nil {
+		return nil, err
+	}
+	idBytes := make([]byte, idLen)
+	if _, err := io.ReadFull(buf, idBytes); err != nil {
+		return nil, err
+	}
+	t.ID = string(idBytes)
+
+	// Read Info
+	var infoLen uint32
+	if err := binary.Read(buf, binary.LittleEndian, &infoLen); err != nil {
+		return nil, err
+	}
+	t.Info = make(json.RawMessage, infoLen)
+	if _, err := io.ReadFull(buf, t.Info); err != nil {
+		return nil, err
+	}
+
+	// Read Signature
+	var sigLen uint32
+	if err := binary.Read(buf, binary.LittleEndian, &sigLen); err != nil {
+		return nil, err
+	}
+	t.Signature = make(json.RawMessage, sigLen)
+	if _, err := io.ReadFull(buf, t.Signature); err != nil {
+		return nil, err
+	}
+
+	return t, nil
 }
