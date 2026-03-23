81f18ca5e9e971f326c6a45be6981fc6dc493fcc
Maneesha Panda <maneesha.p@rubix.net>
2026-03-23 12:14:09 +0530

    resolve import error from crypto/sha3 package

--- util/crypto.go
+++ util/crypto.go
@@ -1,18 +1,17 @@
 package util
 
 import (
-	"crypto/sha3"
-
 	"github.com/rubixchain/rubixgoplatform/constants"
+	"golang.org/x/crypto/sha3"
 )
- 
+
 func CalculateHash(data []byte, method string) []byte {
-    switch method {
-    case constants.HashAlgorithm_SHA3_256:
-        h := sha3.New256()
-        h.Write(data)
-        return h.Sum(nil)
-    default:
-        return nil
-    }
+	switch method {
+	case constants.HashAlgorithm_SHA3_256:
+		h := sha3.New256()
+		h.Write(data)
+		return h.Sum(nil)
+	default:
+		return nil
+	}
 }
