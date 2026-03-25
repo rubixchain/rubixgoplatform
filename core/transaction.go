025ec7d4f0d87ecb572361b4596ba1f2766e1c86
Arnab Ghose <arnab.ghose128@gmail.com>
2026-03-25 17:10:29 +0530

    refactored FT and Transactions server package APIs

--- core/transaction.go
+++ core/transaction.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/rubixchain/rubixgoplatform/core/ipfsport"
@@ -266,4 +268,22 @@ func (c *Core) SendTokens(request *ensweb.Request) *ensweb.Result {
 	}
 
 	return c.l.RenderJSON(request, &crep, http.StatusBadRequest)
+}
+
+func (c *Core) GetTransactionByID(txId string) (*models.TransactionInfo, error) {
+	transactionDetail, err := c.w.GetTransactionByID(txId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get transactions details for tx: %v, err: %v", txId, err)
+	}
+
+	var txInfo *models.TransactionInfo
+	if err := json.Unmarshal(transactionDetail.Info, txInfo); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal transaction info, err: %v", err)
+	}
+
+	return txInfo, nil
+}
+
+func (c *Core) GetAllTransactions() ([]models.Transactions, error) {
+	return c.w.GetAllTransactions()
 }
--- setup/setup.go
+++ setup/setup.go
@@ -75,7 +75,7 @@ const (
 	APIValidateTokenChain               string = "/api/validate-token-chain"
 	APIGenerateFaucetTestToken          string = "/api/generate-faucettest-token"
 	APIFaucetTokenCheck                 string = "/api/faucet-token-check"
-	APICreateFT                         string = "/api/create-ft"
+	APICreateFT                         string = "/rubix/v1/fts/mint"
 	APIDumpFTTokenChainBlock            string = "/api/dump-ft-token-chain"
 	APIInitiateFTTransfer               string = "/api/initiate-ft-transfer"
 	APIGetFTInfo                        string = "/api/get-ft-info-by-did"
@@ -107,26 +107,17 @@ const (
 	APIRemoteRecoverTokens              string = "/api/remote-recover-tokens"
 
 	//Below are Explorer-service API endpoints
-	APIGetAllRBTs           string = "/api/de-exp/get-rbt-list"
-	APIGetAllFTs            string = "/api/de-exp/get-ft-list"
-	APIGetAllNFTs           string = "/api/de-exp/get-nft-list"
-	APIGetAllSmartContracts string = "/api/de-exp/get-smart-contract-list"
-
-	APIGetRBTbyDID           string = "/api/de-exp/get-rbt-list-by-did"
-	APIGetFTbyDID            string = "/api/de-exp/get-ft-list-by-did"
-	APIGetNFTbyDID           string = "/api/de-exp/get-nft-list-by-did"
-	APIGetSmartContractbyDID string = "/api/de-exp/get-smart-contract-list-by-did"
-	APIGetTxnAmount          string = "/api/de-exp/get-txn-amount-by-txnID"
-
-	APIGetTokenChain       string = "/api/de-exp/get-token-chain"
-	APIGetTokenChainHeight string = "/api/de-exp/get-token-chain-height"
 
 	APINotifyDeExpBlockUpdate string = "/api/block-update"
 	APINotifyDeExpTokenUpdate string = "/api/token-update"
 
 	APIRemoveStaleDID string = "/api/remove-stale-did"
 
-	APIInitiateTransaction string = "/rubix/v1/token/tx"
+	APITransaction        string = "/rubix/v1/tx"
+	APIGetTransactionByID string = "/rubix/v1/tx/{tx_id}"
+
+	// New endpoints
+	APIListFT string = "/rubix/v1/fts"
 )
 
 // jwt.RegisteredClaims
