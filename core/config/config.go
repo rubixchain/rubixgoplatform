6f29b47c5a6689e2615ce32a25459c303e8a8318
Allen Iype P Cherian <allen_iype_p_cherian@outlook.com>
2026-05-11 12:22:55 +0530

    update mainnet and testnet bootstrap nodes

--- core/config/config.go
+++ core/config/config.go
@@ -34,15 +34,10 @@ statement_timeout_seconds = 20
 
 [ipfs]
 mainnet_bootstrap_nodes = [
-	"/ip4/161.35.169.251/tcp/4001/p2p/12D3KooWPhZEYEw4jG3kSRuwgMEHcVt7KMkm1ui2ddu4fgSgwvDq", 
-	"/ip4/103.127.158.120/tcp/4001/p2p/12D3KooWSQ94HRDzFf6W2rp7P8gzP6efZQHTaSU8uaQjskVBHiWP", 
-	"/ip4/172.104.191.191/tcp/4001/p2p/12D3KooWFudnWZY1v1m4YXCzDWZSbNt7nvf5F42uzM6vErZ4NwqJ",
+	"/ip4/172.188.67.118/tcp/4001/p2p/12D3KooWMvgneC9omYaHyKVkFUf1wKCqtMnVqEopTGT1CTGacUEm",
 ]
 testnet_bootstrap_nodes = [
-	"/ip4/103.209.145.177/tcp/4001/p2p/12D3KooWD8Rw7Fwo4n7QdXTCjbh6fua8dTqjXBvorNz3bu7d9xMc", 
-	"/ip4/98.70.52.158/tcp/4001/p2p/12D3KooWQyWFABF3CKFnzX85hf5ZwrT5zPsy4rWHdGPZ8bBpRVCK", 
-	"/ip4/20.244.16.143/tcp/4001/p2p/12D3KooWAydFDJeSW5qupmp3AjRxc82Dq1AnjfJT1zwy4hg2TuNn", 
-	"/ip4/40.81.232.217/tcp/4001/p2p/12D3KooWK6V21GQotbub3cfgb5qAK1uUoUGPexf3vsLqw6yBJfen",
+	"/ip4/103.209.145.177/tcp/4011/p2p/12D3KooWKAHAYEjjckeWi2s3oCnvkbTJX3x6fV3HZbjMEVzMfJeL", 
 ]
 localnet_bootstrap_nodes = []
 `
